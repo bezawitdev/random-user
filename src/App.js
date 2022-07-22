774c61eb050bb7f873e5e686feaee5336a979fad
bezawitdev <getbezawit@gmail.com>
2022-07-21 23:10:31 -0400

    App.js commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
+import { fetchUser } from './services/rando_user';
+import UserSummary from './components/UserSummary';
+
+
 
 function App() {
+  const [currentUser, setCurrentUser] = useState([])
+  const [checkData, setCheckData] = useState(false)
+
+async function refreshUser(){
+  const userList = await fetchUser()
+    // console.log(userList)
+    console.log('It works')
+    setCurrentUser(userList.data.results)
+    setCheckData(true)
+   
+}
+ console.log(checkData, 'checkdata')
+console.log(currentUser, 'app.js first')
+
+useEffect(() => {
+  refreshUser()
+}, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Rnadom User App</h1>
+     <button onClick={refreshUser}>Get User</button>
+    { checkData && <UserSummary userData={currentUser} />}
+    
+     
+     {/* {JSON.stringify(currentUser)} */}
     </div>
   );
 }
